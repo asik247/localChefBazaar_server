2a25e4903bcb3136e0701b6cba702041e6473f24
Asikur Rahaman <mdasikur5893@gmail.com>
2026-06-13 22:32:55 +0600

    middleware code write

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+//Todo middleware here;
+app.use(cors());
+app.use(express.json());
 //Todo root apis here;
 app.get('/', (req, res) => {
   res.send('Hello World!');
